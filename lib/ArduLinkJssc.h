94b60d759d028cf2aba45aff1c75ca04af7ad10c
gheraibia <gheraibia@hotmail.com>
2019-01-12 04:15:56 +0100

    Add files via upload

--- lib/ArduLinkJssc.h
+++ lib/ArduLinkJssc.h
@@ -7,19 +7,19 @@ class ArduLinkJssc
 	ArduLinkJssc(HardwareSerial*);
 	void eventSerialListener();
 	void parseMessage();
-	bool getMsgState() const;
+  bool getMsgState() const;
 	bool haveToResponds() const;
 	void response();
 	int  getType() const;
 	void setCustomDeal(void (*fct)(String commande,String value));
   
 	private:
-  	void flushMsg();
+  void flushMsg();
 	void (*custDeal)(String commande,String value);
 	int idPosition;
 	bool msgRecognized;
 	bool answerWait;
 	String _inputString;         
- 	boolean _stringComplete;
-  	HardwareSerial *_serial;
+  boolean _stringComplete;
+  HardwareSerial *_serial;
 };
