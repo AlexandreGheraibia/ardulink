6daf6e16b2cad580ac1348d9b51b832e2bab0fa9
gheraibia <gheraibia@hotmail.com>
2019-01-12 04:31:00 +0100

    Update ArduLinkJssc.h

--- lib/ArduLinkJssc.h
+++ lib/ArduLinkJssc.h
@@ -3,22 +3,22 @@
     
 class ArduLinkJssc
 {
-	public:
+  public:
 	ArduLinkJssc(HardwareSerial*);
 	void eventSerialListener();
 	void parseMessage();
 	void setCustomDeal(void (*fct)(String commande,String value));
-  bool getMsgState() const;
-	private:
- 
-  bool haveToResponds() const;
-  void flushMsg();
-  void response();
+ 	bool getMsgState() const;
+	
+  private:
+  	bool haveToResponds() const;
+  	void flushMsg();
+  	void response();
 	void (*custDeal)(String commande,String value);
 	int idPosition;
 	bool msgRecognized;
 	bool answerWait;
 	String _inputString;         
-  boolean _stringComplete;
-  HardwareSerial *_serial;
+	boolean _stringComplete;
+  	HardwareSerial *_serial;
 };
