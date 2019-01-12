e89079274acbe08a878aeb27d93ef2a6ac81bd98
gheraibia <gheraibia@hotmail.com>
2019-01-12 05:36:07 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+import org.ardulink.core.Link;
+import org.ardulink.core.events.CustomEvent;
+import org.ardulink.core.events.CustomListener;
+
+
+public class Main{
+//receive message from the Arduino Card    
+static private CustomListener eventCustListener() {
+  return new CustomListener() {
+	  			@Override
+	  	  	public	void customEventReceived(CustomEvent e) {
+						System.out.println(e.getMessage());
+					}
+  }
+}
+
+public static void main(String[] args) throws InterruptedException {
+	  Link link=Links.getDefault();
+    if(link!=null) {
+    //send message to thz Arduino Card    
+    link.sendCustomMessage("b/value");
+    }
+	
+	}
+}
