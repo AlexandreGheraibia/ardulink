6e91fef7b9d045e30859975ab2836b1106ffca9f
gheraibia <gheraibia@hotmail.com>
2019-01-21 14:01:20 +0100

    Update StateCardManager.h

--- 4digits/sketch Arduino/StateCardManager.h
+++ 4digits/sketch Arduino/StateCardManager.h
@@ -5,15 +5,15 @@ class StateCardManager{
 	StateCardManager(HardwareSerial *,String,String,float,long);
 	void sendCurrentData();
 	void setCurrentMessage(String);
-  void setCurrentMotorAngle(float);
-  void setCurrentDisplaySpeed(long);
+ 	void setCurrentMotorAngle(float);
+  	void setCurrentDisplaySpeed(long);
 	void setAutreMessage(String);
 	char* getCurrentMsg() const;
-  bool isStateChange() const;
+  	bool isStateChange() const;
 	private:
 	HardwareSerial * _serial=NULL;
-  float angle=0.0f;
-  long dSpeed=0;
+  	float angle=0.0f;
+  	long dSpeed=0;
 	char currentMsg[50];
 	char instMsg[50];
 	bool stateChange=false;
